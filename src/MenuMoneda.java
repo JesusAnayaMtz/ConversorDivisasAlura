188dd3aae4083397807de9b481cf47df76c012ac
Jesus Anaya Mtz <118866969+systemspool@users.noreply.github.com>
2024-04-29 17:20:51 -0600

    SE AGREGAN EXCEPCIONES
    se agregan excepciones al momento de selecciona la opcion o la cantidad a convertir

--- src/MenuMoneda.java
+++ src/MenuMoneda.java
@@ -11,50 +11,54 @@ public class MenuMoneda {
         int salir = 1;
         System.out.println("Bienvenido/a al conversor de moneda");
         System.out.println();
-        while (salir != 0){
-            System.out.println("1) Dolar =>> Peso Mexicano\n2) Peso Mexicano =>> Dolar\n3) Dolar =>> Euro\n" +
-                    "4) Euro =>> Dolar\n5) Dolar =>> Libra Esterlina\n6) Libra Esterlina =>> Dolar\n" +
-                    "7) Salir");
-            System.out.println("Ingrese la opcion deseada");
-            var opcion = leer.nextInt();
-            switch (opcion){
-                case 1:
-                    tipoCambio = "usd/mxn/";
-                    break;
-                    case 2:
-                        tipoCambio = "mxn/usd/";
+        try {
+            while (salir != 0){
+                System.out.println("1) Dolar =>> Peso Mexicano\n2) Peso Mexicano =>> Dolar\n3) Dolar =>> Euro\n" +
+                        "4) Euro =>> Dolar\n5) Dolar =>> Libra Esterlina\n6) Libra Esterlina =>> Dolar\n" +
+                        "7) Salir");
+                System.out.println("Ingrese la opcion deseada");
+                var opcion = leer.nextInt();
+                switch (opcion){
+                    case 1:
+                        tipoCambio = "usd/mxn/";
                         break;
-                        case 3:
-                            tipoCambio = "usd/eur/";
+                        case 2:
+                            tipoCambio = "mxn/usd/";
                             break;
-                            case 4:
-                                tipoCambio = "eur/usd/";
+                            case 3:
+                                tipoCambio = "usd/eur/";
                                 break;
-                                case 5:
-                                     tipoCambio = "usd/gbp/";
+                                case 4:
+                                    tipoCambio = "eur/usd/";
                                     break;
-                                    case 6:
-                                         tipoCambio = "gbp/usd/";
+                                    case 5:
+                                         tipoCambio = "usd/gbp/";
                                         break;
-                                        case 7:
-                                            System.out.println("Saliste del sistema");
-                                            salir=0;
+                                        case 6:
+                                             tipoCambio = "gbp/usd/";
                                             break;
-                default:
-                    System.out.println("opcion no valida seleccione una opcion de la lista");
-                    break;
-            }
-            System.out.println("Ingrese la cantidad a cambiar");
-            cantidad = leer.nextDouble();
-            moneda = consultaMoneda.ConsultaMoneda(tipoCambio, cantidad);
-            System.out.println("El valor de " + cantidad + " " + moneda.base_code() + " a " +moneda.target_code() + " es igual a " + moneda.conversion_result() + " " + moneda.target_code()  + " al dia de hoy");
-            System.out.println("1) Desea Consultar Otro Tipo De Cambio----2) Salir");
-            salir = leer.nextInt();
-            if (salir == 2){
-                System.out.println("Saliste del sistema");
-                salir = 0;
-            }
+                                            case 7:
+                                                System.out.println("Saliste del sistema");
+                                                salir=0;
+                                                break;
+                    default:
+                        System.out.println("opcion no valida seleccione una opcion de la lista");
+                        break;
+                }
+                System.out.println("Ingrese la cantidad a cambiar");
+                cantidad = leer.nextDouble();
+                moneda = consultaMoneda.ConsultaMoneda(tipoCambio, cantidad);
+                System.out.println("El valor de " + cantidad + " " + moneda.base_code() + " a " +moneda.target_code() + " es igual a " + moneda.conversion_result() + " " + moneda.target_code()  + " al dia de hoy");
+                System.out.println("1) Desea Consultar Otro Tipo De Cambio----2) Salir");
+                salir = leer.nextInt();
+                if (salir == 2){
+                    System.out.println("Saliste del sistema");
+                    salir = 0;
+                }
 
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Ingrese una opcion numerica o cantidad valida");
         }
     }
 }
